f27d79b8820b293a3c46cb3cfbc4bc5fdc4d49bb
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-19 17:21:52 +0800

    chore: use iconify

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <UiButton>Click me</UiButton>
+    <UiCard class="w-[350px]">
+        <UiCardHeader>
+            <UiCardTitle>Cart Test</UiCardTitle>
+        </UiCardHeader>
+        <UiCardContent>
+            <UiButton>
+                <span class="icon-[tabler--align-box-bottom-left]"></span>
+                Icon & Button test
+            </UiButton>
+        </UiCardContent>
+    </UiCard>
 </template>
 
 <script setup lang="ts">
