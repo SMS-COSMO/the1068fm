c6782a1159c02b96a117b64549acb2f58352dfc8
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-20 22:26:28 +0800

    feat(manage): first commit

---
+++ pages/manage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex flex-row gap-5 h-screen p-5">
+    <UiCard class="basis-1/3">
+      <UiCardHeader>
+        <UiCardTitle>
+          Calendar
+        </UiCardTitle>
+        <UiCardDescription>
+          placeholder
+        </UiCardDescription>
+      </UiCardHeader>
+      <UiCardContent>
+        aa
+      </UiCardContent>
+    </UiCard>
+    <UiCard class="basis-1/3 relative">
+      <UiCardHeader>
+        <UiCardTitle>
+          本周排歌表
+        </UiCardTitle>
+        <UiCardDescription>
+          placeholder
+        </UiCardDescription>
+      </UiCardHeader>
+      <UiCardContent>
+        <MusicCard title="dsd" creator="aa" editable></MusicCard>
+      </UiCardContent>
+      <UiButton class="absolute right-5 bottom-5">
+        保存修改
+      </UiButton>
+    </UiCard>
+    <UiCard class="basis-1/3">
+      <UiCardHeader>
+        <UiCardTitle>
+          总歌单
+        </UiCardTitle>
+        <UiCardDescription>
+          placeholder
+        </UiCardDescription>
+      </UiCardHeader>
+      <UiCardContent>
+        <UiTabs>
+          <UiTabsList default-value="unset" class="grid grid-cols-3">
+            <UiTabsTrigger value="unset">
+              待审核
+            </UiTabsTrigger>
+            <UiTabsTrigger value="rejected">
+              已拒绝
+            </UiTabsTrigger>
+            <UiTabsTrigger value="used">
+              已使用
+            </UiTabsTrigger>
+          </UiTabsList>
+          <UiTabsContent value="unset">
+            Unset
+          </UiTabsContent>
+          <UiTabsContent value="rejected">
+            Rejected
+          </UiTabsContent>
+          <UiTabsContent value="used">
+            Used
+          </UiTabsContent>
+        </UiTabs>
+      </UiCardContent>
+    </UiCard>
+  </div>
+</template>
