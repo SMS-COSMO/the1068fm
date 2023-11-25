d9d225486af178bfd8e19a3d937e1ed738687732
AsyncFox <dev.asyncfox@gmail.com>
2023-11-25 08:51:34 +0800

    fix: trpc zod errorFormatter

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,9 +1,23 @@
 import { TRPCError, initTRPC } from '@trpc/server'
 import { Context } from '~/server/trpc/context'
 import superjson from 'superjson'
+import { ZodError } from 'zod';
 
 const t = initTRPC.context<Context>().create({
     transformer: superjson,
+    errorFormatter(opts) {
+        const { shape, error } = opts;
+        return {
+            ...shape,
+            data: {
+                ...shape.data,
+                zodError:
+                    error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
+                        ? error.cause.errors
+                        : null,
+            },
+        };
+    },
 })
 
 export const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
