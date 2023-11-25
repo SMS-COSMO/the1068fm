20b35b41f11370e227d4f2a64d3d5943489c5094
AsyncFox <dev.asyncfox@gmail.com>
2023-11-25 22:52:06 +0800

    feat: finish submit page

--- pages/submit.vue
+++ pages/submit.vue
@@ -10,7 +10,32 @@
         </UiCardTitle>
       </UiCardHeader>
       <UiCardContent>
-        <form @submit="onSubmit">
+        <form @submit="onSubmit" class="grid grid-cols-1 gap-4">
+
+          <UiFormField v-slot="{ componentField }" name="type">
+            <UiFormItem>
+              <UiFormLabel>投稿类型</UiFormLabel>
+              <UiSelect v-bind="componentField">
+                <UiFormControl>
+                  <UiSelectTrigger>
+                    <UiSelectValue placeholder="请选择一种类型" />
+                  </UiSelectTrigger>
+                </UiFormControl>
+                <UiSelectContent>
+                  <UiSelectGroup>
+                    <UiSelectItem value="normal">
+                      001 一般投稿
+                    </UiSelectItem>
+                    <UiSelectItem value="withMsg">
+                      002 带留言投稿
+                    </UiSelectItem>
+                  </UiSelectGroup>
+                </UiSelectContent>
+              </UiSelect>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
           <UiFormField v-slot="{ componentField }" name="name">
             <UiFormItem>
               <UiFormLabel>歌曲名</UiFormLabel>
@@ -20,51 +45,59 @@
               <UiFormMessage />
             </UiFormItem>
           </UiFormField>
-          <div class="mt-3">
-            <UiFormField v-slot="{ componentField }" name="creator">
-              <UiFormItem>
-                <UiFormLabel>歌手</UiFormLabel>
-                <UiFormControl>
-                  <UiInput type="text" v-bind="componentField" />
-                </UiFormControl>
-                <UiFormMessage />
-              </UiFormItem>
-            </UiFormField>
-          </div>
-          <div class="mt-3">
-            <UiFormField v-slot="{ componentField }" name="submitterName">
-              <UiFormItem>
-                <UiFormLabel>提交者</UiFormLabel>
-                <UiFormControl>
-                  <UiInput type="text" v-bind="componentField" />
-                </UiFormControl>
-                <UiFormMessage />
-              </UiFormItem>
-            </UiFormField>
-          </div>
-          <div class="mt-3">
-            <UiFormField v-slot="{ componentField }" name="submitterGrade">
-              <UiFormItem>
-                <UiFormLabel>年级</UiFormLabel>
-                <UiFormControl>
-                  <UiInput type="text" v-bind="componentField" />
-                </UiFormControl>
-                <UiFormMessage />
-              </UiFormItem>
-            </UiFormField>
-          </div>
-          <div class="mt-3">
-            <UiFormField v-slot="{ componentField }" name="submitterClass">
-              <UiFormItem>
-                <UiFormLabel>班级</UiFormLabel>
-                <UiFormControl>
-                  <UiInput type="number" v-bind="componentField" />
-                </UiFormControl>
-                <UiFormMessage />
-              </UiFormItem>
-            </UiFormField>
-          </div>
-          <UiButton type="submit" class="mt-3" :disabled="buttonLoading">
+
+          <UiFormField v-slot="{ componentField }" name="creator">
+            <UiFormItem>
+              <UiFormLabel>歌手</UiFormLabel>
+              <UiFormControl>
+                <UiInput type="text" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
+          <UiFormField v-slot="{ componentField }" name="submitterName">
+            <UiFormItem>
+              <UiFormLabel>提交者姓名</UiFormLabel>
+              <UiFormControl>
+                <UiInput type="text" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
+          <UiFormField v-slot="{ componentField }" name="submitterGrade">
+            <UiFormItem>
+              <UiFormLabel>年级（请填1或2）</UiFormLabel>
+              <UiFormControl>
+                <UiInput type="number" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
+          <UiFormField v-slot="{ componentField }" name="submitterClass">
+            <UiFormItem>
+              <UiFormLabel>班级（请填数字）</UiFormLabel>
+              <UiFormControl>
+                <UiInput type="number" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
+          <UiFormField v-slot="{ componentField }" name="message" v-if="values.type === 'withMsg'">
+            <UiFormItem>
+              <UiFormLabel>投稿留言</UiFormLabel>
+              <UiFormControl>
+                <UiInput type="text" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
+
+          <UiButton type="submit" class="mt-3 ml-auto px-6 font-bold text-md flex items-center justify-center"
+            :disabled="buttonLoading">
             <Loader2 class="w-4 h-4 mr-2 animate-spin" v-show="buttonLoading" />
             提交
           </UiButton>
@@ -75,26 +108,45 @@
 </template>
 
 <script setup lang="ts">
-import { ChevronLeft } from 'lucide-vue-next';
+import { ChevronLeft, Loader2 } from 'lucide-vue-next';
 import { useForm } from 'vee-validate';
 import { toTypedSchema } from '@vee-validate/zod';
-import * as z from 'zod';
+import { z } from 'zod';
+
+definePageMeta({
+  pageTransition: {
+    name: 'slide-left',
+    mode: 'out-in'
+  }
+})
+
+const { $toast, $api } = useNuxtApp()
 
 const formSchema = toTypedSchema(z.object({
-  name: z.string().min(1, '歌名长度至少为1').max(50, '歌名长度最大为50'),
-  creator: z.string().min(1, '歌手名长度至少为1').max(50, '歌手长度最大为50'),
-  submitterName: z.string().min(2, '申请者名字长度至少为2').max(15, '申请者名字长度最大为15'),
-  submitterGrade: z.number().min(1000).max(5000), // problem for people in 5000s
-  submitterClass: z.number().min(0, '班级号应大于0').max(100, '班级号应小于100'),
+  name: z.string({ required_error: '歌名长度至少为1' }).min(1, '歌名长度至少为1').max(50, '歌名长度最大为50'),
+  creator: z.string({ required_error: '歌手名长度至少为1' }).min(1, '歌手名长度至少为1').max(50, '歌手长度最大为50'),
+  submitterName: z.string({ required_error: '提交者名字长度至少为2' }).min(2, '提交者名字长度至少为2').max(15, '提交者名字长度最大为15'),
+  submitterGrade: z.coerce.number({ invalid_type_error: '请填一个数字' }).int('请填一个整数').min(1, '年级为1或2').max(2, '年级为1或2'), // problem for people in 5000s
+  submitterClass: z.coerce.number({ invalid_type_error: '请填一个数字' }).min(0, '班级号应大于0').max(100, '班级号应小于100'),
   type: z.enum(['normal', 'withMsg'], { errorMap: () => ({ message: '提交了不存在的歌曲类型' }) }),
+  message: z.string().nullish(),
 }));
 
-const form = useForm({
+const { handleSubmit, values } = useForm({
   validationSchema: formSchema,
 });
 
-const buttonLoading = ref(false);
-const onSubmit = form.handleSubmit(async values => { });
+const [buttonLoading, toggleLoading] = useToggle(false);
+const onSubmit = handleSubmit(async (values) => {
+  toggleLoading()
+  try {
+    await $api.song.create.mutate(values)
+    $toast.success('提交成功！')
+  } catch (err) {
+    useErrorHandler(err)
+  }
+  toggleLoading()
+})
 
 const back = () => {
   const router = useRouter();
