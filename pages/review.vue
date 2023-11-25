3a9f6096eaeac070a28b48edd4e0ba33da1cdd63
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-25 14:00:56 +0800

    feat(review): add approved bar

--- pages/review.vue
+++ pages/review.vue
@@ -1,14 +1,17 @@
 
 <template>
   <div class="flex flex-row gap-5 h-screen p-5">
-    <UiCard class="basis-1/4 pt-4">
+    <UiCard class="basis-1/4 pt-4 relative">
       <UiCardHeader>
-        <UiCardTitle>
+        <UiCardTitle class="flex">
           待审核
+          <UiBadge variant="secondary" class="ml-2 self-center">
+            {{ unsetList.length }}
+          </UiBadge>
         </UiCardTitle>
       </UiCardHeader>
       <UiCardContent>
-        <UiScrollArea class="h-[calc(100vh-18rem)]">
+        <UiScrollArea class="h-[calc(100vh-10rem)]">
           <div v-for="(song, index) in unsetList" :key="index">
             <UiContextMenu>
               <UiContextMenuTrigger>
@@ -23,19 +26,58 @@
           </div>
         </UiScrollArea>
       </UiCardContent>
+      <UiPopover v-model:open="rejectOpen">
+        <UiPopoverTrigger as-child>
+          <UiButton @click="rejectOpen = true" variant="outline" class="absolute right-5 bottom-5">
+            全部拒绝
+          </UiButton>
+        </UiPopoverTrigger>
+        <UiPopoverContent class="w-60 mr-2 mb-2">
+          <UiCard class="border-0 shadow-transparent">
+            <UiCardHeader class="p-2">
+              <UiCardTitle class="text-xl">
+                确定要全部拒绝吗？
+              </UiCardTitle>
+              <UiCardDescription>
+                此操作不可撤销
+              </UiCardDescription>
+            </UiCardHeader>
+            <UiCardContent class="p-2">
+              <div class="ml-auto">
+                <UiButton @click="rejectAll" variant="destructive">
+                  是
+                </UiButton>
+                <UiButton @click="rejectOpen = false" variant="outline" class="ml-2">
+                  取消
+                </UiButton>
+              </div>
+            </UiCardContent>
+          </UiCard>
+        </UiPopoverContent>
+      </UiPopover>
     </UiCard>
     <UiCard class="basis-1/2 pt-4">
-      <UiCardHeader>
+      <UiCardHeader class="items-start gap-4 space-y-0 flex-row">
         <UiCardTitle>
           歌曲试听
         </UiCardTitle>
+        <div v-if="selectedSong" class="ml-auto h-4">
+          <UiButton variant="secondary" size="icon" @click="reviewSong(selectedSong, 'approved');"
+            class="w-20 my-[-10px] hover:bg-green-200 hover:border-green-400 hover:text-green-700">
+            <Check class="w-5 h-5" />
+          </UiButton>
+          <UiButton variant="secondary" size="icon" @click="reviewSong(selectedSong, 'rejected');"
+            class="w-20 ml-3 my-[-10px] hover:bg-red-200 hover:border-red-400 hover:text-red-700">
+            <X class="w-5 h-5" />
+          </UiButton>
+        </div>
       </UiCardHeader>
       <UiCardContent>
         <UiScrollArea class="h-[calc(100vh-10rem)]">
           <div v-if="!searchLoading" v-for="song in searchList" :key="song">
             <MusicPlayerCard :song="song" />
           </div>
-          <UiCard v-else v-for="n in 10" :key="n" class="flex items-center space-x-4 mb-4">
+          <UiCard v-if="searchLoading && selectedSong" v-for="n in 10" :key="n" class="flex items-center space-x-4 mb-4">
             <UiCardHeader class="items-start gap-4 space-y-0 flex-row w-full">
               <UiSkeleton class="h-20 w-20 rounded-sm" />
               <div class="space-y-2 w-full">
@@ -49,12 +91,70 @@
       </UiCardContent>
     </UiCard>
     <UiCard class="basis-1/4 pt-4">
-
+      <UiCardHeader class="items-start gap-4 space-y-0 flex-row">
+        <div class="space-y-1">
+          <UiCardTitle>
+            已审核
+          </UiCardTitle>
+        </div>
+        <UiButton @click="enterManage" variant="secondary" class="self-center my-[-10px] ml-auto">
+          返回排歌模式
+        </UiButton>
+      </UiCardHeader>
+      <UiCardContent>
+        <UiTabs default-value="approved">
+          <UiTabsList class="grid grid-cols-2">
+            <UiTabsTrigger value="approved">
+              已通过
+              <UiBadge variant="secondary" class="ml-2">
+                {{ approvedList.length }}
+              </UiBadge>
+            </UiTabsTrigger>
+            <UiTabsTrigger value="rejected">
+              已拒绝
+              <UiBadge variant="secondary" class="ml-2">
+                {{ rejectedList.length }}
+              </UiBadge>
+            </UiTabsTrigger>
+          </UiTabsList>
+          <UiTabsContent value="approved">
+            <UiScrollArea class="h-[calc(100vh-14rem)]">
+              <div v-for="(song, index) in approvedList" :key="index">
+                <UiContextMenu>
+                  <UiContextMenuTrigger>
+                    <MusicCard :song="song" />
+                  </UiContextMenuTrigger>
+                  <UiContextMenuContent>
+                    <UiContextMenuItem @click="updateSong(song, 'rejected')">拒绝</UiContextMenuItem>
+                    <UiContextMenuItem @click="updateSong(song, 'unset')">移入待审核</UiContextMenuItem>
+                  </UiContextMenuContent>
+                </UiContextMenu>
+              </div>
+            </UiScrollArea>
+          </UiTabsContent>
+          <UiTabsContent value="rejected">
+            <UiScrollArea class="h-[calc(100vh-14rem)]">
+              <div v-for="(song, index) in rejectedList" :key="index">
+                <UiContextMenu>
+                  <UiContextMenuTrigger>
+                    <MusicCard :song="song" />
+                  </UiContextMenuTrigger>
+                  <UiContextMenuContent>
+                    <UiContextMenuItem @click="updateSong(song, 'approved')">通过</UiContextMenuItem>
+                    <UiContextMenuItem @click="updateSong(song, 'unset')">移入待审核</UiContextMenuItem>
+                  </UiContextMenuContent>
+                </UiContextMenu>
+              </div>
+            </UiScrollArea>
+          </UiTabsContent>
+        </UiTabs>
+      </UiCardContent>
     </UiCard>
   </div>
 </template>
 
 <script setup lang="ts">
+import { X, Check } from 'lucide-vue-next';
 import { isTRPCClientError } from '~/lib/utils';
 import type { TSong, TSongList } from '~/lib/utils';
 const { $api, $toast } = useNuxtApp();
@@ -66,6 +166,8 @@ const trpcErr = (err: unknown) => {
     $toast.error('未知错误');
   }
 };
+
+const rejectOpen = ref(false);
 
 const songList = ref<TSongList>([]);
 const unsetList = computed(
@@ -104,6 +206,12 @@ const searchList = computedAsync(
   },
 );
 
+const rejectAll = async () => {
+  rejectOpen.value = false;
+  for (let song of songList.value)
+    await updateSong(song, 'rejected');
+};
+
 const updateSong = async (song: TSong, status: 'unset' | 'approved' | 'rejected' | 'used') => {
   try {
     await $api.song.modifyStatus.mutate({ id: song.id, status });
@@ -112,6 +220,16 @@ const updateSong = async (song: TSong, status: 'unset' | 'approved' | 'rejected'
   } catch (err) {
     trpcErr(err);
   }
+};
+
+const reviewSong = async (song: TSong, status: 'unset' | 'approved' | 'rejected' | 'used') => {
+  updateSong(song, status);
+  selectedSong.value = unsetList.value[unsetList.value.indexOf(song) + 1];
+};
+
+const enterManage = () => {
+  const router = useRouter();
+  router.push('/manage');
 };
 
 onMounted(async () => {
