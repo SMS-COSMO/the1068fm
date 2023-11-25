5d5667f5c899e0af1829bd84ffc461ae1708bf39
AsyncFox <dev.asyncfox@gmail.com>
2023-11-25 22:40:38 +0800

    feat: add transition

--- app.vue
+++ app.vue
@@ -8,3 +8,28 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all 0.2s;
+}
+.slide-left-enter-from {
+  opacity: 0;
+  transform: translate(50px, 0);
+}
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translate(-50px, 0);
+}
+.slide-right-enter-from {
+  opacity: 0;
+  transform: translate(-50px, 0);
+}
+.slide-right-leave-to {
+  opacity: 0;
+  transform: translate(50px, 0);
+}
+</style>
