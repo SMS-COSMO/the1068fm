2b4e6aa4db4e28e6bd251bb42b86b4a4fe21ae16
AsyncFox <dev.asyncfox@gmail.com>
2023-11-25 22:46:26 +0800

    fix: error message not showing correctly

--- composables/useErrorHandler.ts
+++ composables/useErrorHandler.ts
@@ -5,7 +5,7 @@ export const useErrorHandler = async (err: unknown) => {
       for (const issue of err.data.zodError)
         $toast.error(issue.message);
     }
-    $toast.error(err.message);
+    else $toast.error(err.message);
   } else {
     $toast.error('未知错误');
   }
