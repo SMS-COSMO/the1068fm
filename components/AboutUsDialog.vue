c9048d4d42e2369bad60907d4f888d44b3630bed
AsyncFox <dev.asyncfox@gmail.com>
2023-11-25 22:40:20 +0800

    feat: update index page

---
+++ components/AboutUsDialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <UiDialog>
+        <UiDialogTrigger as-child>
+            <slot></slot>
+        </UiDialogTrigger>
+        <UiDialogContent class="w-[92vw] rounded-md">
+            <UiDialogHeader>
+                <UiDialogTitle class="text-xl font-bold text-start">关于我们</UiDialogTitle>
+            </UiDialogHeader>
+            <div class="flex flex-col my-2">
+                <NuxtImg preload src="/logo.svg" class="mx-auto mb-6"></NuxtImg>
+                <section class="prose">
+                    <p>
+                        <em>the1068fm</em> 是由<b>COSMO智慧校园平台运研中心</b>为<b>风华子衿广播站</b>声声慢点歌节目开发的一体化系统，包含歌曲投稿，歌曲审核，一键排歌等功能。
+                    </p>
+                    <p>
+                        <b>COSMO智慧校园平台运研中心</b>是深圳中学七大校级学生组织之一，也是最年轻的校级学生组织。欢迎大家关注我们的微信公众号！
+                    </p>
+                </section>
+            </div>
+            <UiDialogFooter>
+                <UiDialogClose as-child>
+                    <UiButton type="button" variant="secondary" class="ml-auto px-6">
+                        好的
+                    </UiButton>
+                </UiDialogClose>
+            </UiDialogFooter>
+        </UiDialogContent>
+    </UiDialog>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="mx-9">
-    <div class="font-bold text-4xl mb-10 mt-16">
-      点歌
-    </div>
+  <div class="max-w-[600px] mx-auto">
+    <div class="mx-8">
+      <div class="mb-4 mt-8 flex justify-start">
+        <NuxtImg preload src="/combined-logo.svg" class="w-[72vw]"></NuxtImg>
+      </div>
+      <section class="my-4 grid grid-cols-1 gap-4">
+        <div class="grid grid-cols-2 gap-2">
+          <UiCard class="shadow p-0 pt-1 pb-2">
+            <UiCardHeader class="pt-1 pb-0 text-3xl font-bold">
+              {{ songList.length }}
+            </UiCardHeader>
+            <UiCardContent class="pt-0 pb-0">
+              <span class="text-md">已收集歌曲</span>
+            </UiCardContent>
+          </UiCard>
+          <UiCard class="shadow p-0 pt-1 pb-2">
+            <UiCardHeader class="pt-1 pb-0 text-3xl font-bold">
+              {{ songList.filter((song) => song.status !== 'unset').length }}
+            </UiCardHeader>
+            <UiCardContent class="pt-0 pb-0">
+              <span class="text-md">已审核歌曲</span>
+            </UiCardContent>
+          </UiCard>
+        </div>
+        <div class="grid grid-cols-3 gap-2">
+          <SubmissionRulesDialog>
+            <UiButton type="button" class="w-auto shadow text-md p-0" variant="outline">
+              <span class="icon-[radix-icons--question-mark-circled] w-5 h-5 mr-1" />
+              <span class="align-bottom">
+                规则介绍
+              </span>
+            </UiButton>
+          </SubmissionRulesDialog>
+          <UiButton @click="navigateTo('/submit')" class="w-auto shadow text-md p-0">
+            <Music4 class="w-5 h-5 mr-1" />
+            <span class="align-bottom">
+              歌曲投稿
+            </span>
+          </UiButton>
+          <AboutUsDialog>
+            <UiButton type="button" class="w-auto shadow text-md p-0" variant="outline">
+              <span class="icon-[radix-icons--info-circled] w-5 h-5 mr-1" />
+              <span class="align-bottom">
+                关于我们
+              </span>
+            </UiButton>
+          </AboutUsDialog>
+        </div>
+      </section>
 
-    <div class="flex flex-row gap-5">
-      <UiCard class="h-30 basis-1/2 shadow">
-        <UiCardHeader class="pb-2 text-lg">
-          目前歌曲队列
+      <UiCard class="my-6 shadow">
+        <UiCardHeader>
+          <UiInput placeholder="搜索歌单" class="text-md" />
         </UiCardHeader>
         <UiCardContent>
-          <span class="font-bold text-3xl">10</span>
+          <div v-for="(song, index) in songList.filter(s => (s.status === 'unset'))" :key="index">
+            <MusicCard :song="song"></MusicCard>
+          </div>
         </UiCardContent>
       </UiCard>
-      <UiButton @click="submit" class="h-30 basis-1/2 shadow text-2xl">
-        <span class="icon-[tabler--music] mr-1"></span>
-        歌曲投稿
-      </UiButton>
     </div>
-
-    <UiCard class="my-6 shadow">
-      <UiCardHeader>
-        <UiInput placeholder="搜索歌单" class="text-md" />
-      </UiCardHeader>
-      <UiCardContent>
-        <div v-for="(song, index) in songList.filter(s => (s.status === 'unset'))" :key="index">
-          <MusicCard :song="song"></MusicCard>
-        </div>
-      </UiCardContent>
-    </UiCard>
   </div>
 </template>
 
 <script setup lang="ts">
-import { isTRPCClientError } from '~/lib/utils';
+import { Music4 } from 'lucide-vue-next';
 import type { TSongList } from '~/lib/utils';
-const { $api, $toast } = useNuxtApp();
+const { $api } = useNuxtApp();
 
 const songList = ref<TSongList>([]);
 
-const submit = () => {
-  const router = useRouter();
-  router.push('/submit');
-};
+definePageMeta({
+  pageTransition: {
+    name: 'slide-right',
+    mode: 'out-in'
+  }
+})
+
+useHead({
+  title: 'the1068fm 点歌系统',
+  meta: [
+    { name: 'description', content: 'the1068fm 点歌系统 Made by COSMO.' }
+  ],
+  bodyAttrs: {
+    class: 'm-0 min-w-screen'
+  },
+})
 
 onMounted(async () => {
   try {
     songList.value = (await $api.song.list.query()).sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
   } catch (err) {
-    if (isTRPCClientError(err)) {
-      $toast.error(err.message);
-    } else {
-      $toast.error('未知错误');
-    }
+    useErrorHandler(err)
   }
 });
 </script>
