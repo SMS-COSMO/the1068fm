46883931a13582b99197fa72297c486e22b5debb
ZTL-UwU <zhangtianli2006@163.com>
2024-03-21 13:49:36 +0800

    chore: fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,10 @@
 import type { Config } from 'drizzle-kit';
-import { env } from './server/env';
 
-const dbCredentials = (() => {
-  switch (env.DATABASE_CONNECTION_TYPE) {
-    case 'local': return { url: 'file:local.sqlite' };
-    case 'remote': return { url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN };
-  }
-})();
+const dbCredentials = { url: 'file:local.sqlite' };
 
 export default {
   schema: './server/db/schema/index.ts',
   out: './drizzle',
-  driver: 'turso',
   dbCredentials,
-  tablesFilter: ['!libsql_wasm_func_table'],
+  driver: 'libsql',
 } satisfies Config;
