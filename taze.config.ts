a97cb656d98bab4dfe7cd99c5861a05b5e4514dc
ZTL-UwU <zhangtianli2006@163.com>
2025-04-03 10:07:30 +0800

    chore: bump deps
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ taze.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'taze';
+
+export default defineConfig({
+  exclude: [
+    'tailwind-merge',
+  ],
+  force: true,
+  write: true,
+  install: true,
+  mode: 'major',
+  // ignore paths for looking for package.json in monorepo
+  ignorePaths: [
+    '**/node_modules/**',
+  ],
+});
